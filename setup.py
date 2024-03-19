cc5a713bc3defbde20dd74676903f88234e4972a
Andreas Violaris <48277853+aviolaris@users.noreply.github.com>
2024-03-19 23:49:14 +0200

    Archive repository

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setup(
     name='nordvpn-tray',
-    version='1.0.11',
+    version='1.0.12',
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
     package_data={
@@ -49,7 +49,7 @@ setup(
         ],
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 7 - Inactive',
         'Intended Audience :: End Users/Desktop',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
